3e64e72d376c4982efdc917b68323c2aee5ebdb2
Jack Rugile <jackrugile@gmail.com>
2023-02-22 00:34:57 -0500

    initial files and ignores

---
+++ sanity/sanity.config.js
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Jack Sanity Full',
+
+  projectId: '603awqen',
+  dataset: 'production',
+
+  plugins: [
+    deskTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
